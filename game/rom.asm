d6c6c10749fdfb2f89293d2d586efdf8e0ea5e02
matheus <ra156737@unicamp.br>
2019-08-15 01:20:09 -0300

    novo beep

--- game/rom.asm
+++ game/rom.asm
@@ -168,7 +168,29 @@ ClearMemory:      ; setup ram
 
   ; TODO: remove
   ; eternal beep
+  ;Square 1
+  LDA #%00111000  ;Duty 00, Volume 8 (half volume)
+  STA $4000
+  LDA #$C9        ;$0C9 is a C# in NTSC mode
+  STA $4002       ;low 8 bits of period
+  LDA #$00
+  STA $4003       ;high 3 bits of period
 
+  ;Square 2
+  LDA #%01110110  ;Duty 01, Volume 6
+  STA $4004
+  LDA #$A9        ;$0A9 is an E in NTSC mode
+  STA $4006
+  LDA #$00
+  STA $4007
+
+  ;Triangle
+  LDA #%10000001  ;Triangle channel on
+  STA $4008
+  LDA #$42        ;$042 is a G# in NTSC mode
+  STA $400A
+  LDA #$00
+  STA $400B
 
   JSR WaitVBlank
   ; end of init code
