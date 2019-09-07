57596f6a7509f591fc1b6254b7c52299b99ba988
matheus <ra156737@unicamp.br>
2019-09-06 21:16:22 -0300

    desenho na tela

--- emulator/screen.py
+++ emulator/screen.py
@@ -4,14 +4,44 @@ import time
 
 import pygame
 
-pygame.init()
+# definindo cores
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
-screen = pygame.display.set_mode([640, 480])
+pygame.init() # inicializa objetos
 
-pygame.display.set_caption('Emulator NES')
+screen = pygame.display.set_mode([640, 480]) #tamanho
 
-screen.fill([0, 0, 0])
+# carregando fonte
+font = pygame.font.SysFont(None, 55)
 
+pygame.display.set_caption('Emulator NES') #nome da janela
+
+# preenchendo o fundo com preto
+screen.fill(BLACK)
+
+# desenhando na superfície
+pygame.draw.line(screen, WHITE, [10, 100], [630, 100], 5)
+pygame.draw.rect(screen, BLUE, [200, 210, 40, 20])
+pygame.draw.ellipse(screen, RED, [300, 200, 40, 40])
+pygame.draw.polygon(screen, GREEN, [[420, 200], [440, 240], [400, 240]])
+
+pygame.display.flip() # atualiza a janela
+
+time.sleep(5)
+
+# preenchendo o fundo com preto
+screen.fill(BLACK)
+
+# definindo o texto
+text = font.render('pygame', True, WHITE)
+# copiando o texto para a superfície
+screen.blit(text, [250, 200])
+
+# atualizando a tela
 pygame.display.flip()
 
 time.sleep(5)
