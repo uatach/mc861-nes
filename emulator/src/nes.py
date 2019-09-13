a314d9d40fc0741767d64709dbb466a2e47e83ea
edson duarte <ra145892@students.ic.unicamp.br>
2019-09-12 22:52:44 -0300

    [nes] moved code loading cartridge to nes package

--- emulator/src/nes.py
+++ emulator/src/nes.py
@@ -4,16 +4,31 @@ import logging
 log = logging.getLogger(__name__)
 
 
+def load(data):
+    header = data[:16]
+    banks = header[4]
+    vbanks = header[5]
+    prg_end = 0x4000 * banks + 16
+    chr_end = 0x2000 * vbanks + prg_end
+    prg_rom = data[16:prg_end]
+    chr_rom = data[prg_end:chr_end]
+    return header, prg_rom, chr_rom
+
+
 @attr.s
 class NES(object):
     cpu = attr.ib()
 
     def run(self, data):
         log.info("Running...")
-        log.debug("Cartridge size: %d", len(data))
 
-        prg = None
-        self.cpu.setup(prg)
+        header, prg_rom, chr_rom = load(data)
+        log.debug("Cartridge size: %d", len(data))
+        log.debug("Header size: %d", len(header))
+        log.debug("PRG size: %d", len(prg_rom))
+        log.debug("CHR size: %d", len(chr_rom))
+
+        self.cpu.setup(prg_rom)
 
         while True:
             self.cpu.step()
