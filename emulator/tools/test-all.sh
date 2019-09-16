0e479e9acbf56a7d7f3a1f1646634f5196c33631
edson duarte <ra145892@students.ic.unicamp.br>
2019-09-16 15:24:41 -0300

    [tools] added script to run all tests

---
+++ emulator/tools/test-all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+for filepath in emulator/tst/*
+do
+  filename=$(basename -- "$filepath")
+  name="${filename%.*}"
+
+  out=$(emulator/ext/asm6/asm6 emulator/tst/$filename emulator/bin/$name)
+
+  result=$(pynesemu emulator/bin/$name)
+  expected=$(cat emulator/res/$name.r)
+
+  diff -y --suppress-common-lines <(echo "$result") <(echo "$expected")
+done
