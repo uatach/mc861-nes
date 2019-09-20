2d45b168e97511bbcd6e6581ab09e3b232faaee5
ra14721 <lucas.henrique.waki@gmail.com>
2019-09-20 20:56:08 -0300

    adicionado o código para opcode lsr

--- emulator/src/cpu.py
+++ emulator/src/cpu.py
@@ -95,6 +95,13 @@ class CPU(object):
             0xF6: self._inc_zpx,
             0xF8: self._sed,
             0xFE: self._inc_absx,
+            
+            0xC8: self._iny,
+            0x4A: self._lsr_accumulator,
+            0x46: self._lsr_zeropage,
+            0x56: self._lsr_zeropage_x,
+            0x4E: self._lsr_absolute,
+            0x5E: self._lsr_absolute_x,
         }
         log.info("Handling %d opcodes", len(self.opcodes))
 
@@ -483,3 +490,54 @@ class CPU(object):
         self.sp += 1
         value = self.memory[self.sp]
         return value
+#fazer código para testes - Waki
+    def _iny(self):
+        self.y = (self.y + 1) % 2 ** 8
+        self.__check_flag_zero(self.y)
+        self.__check_flag_negative(self.y)
+
+    def self._lsr_accumulator(self):
+        self.a = (self.a*2)% 2 ** 8
+        #tem que adicionar o overflow
+        self.__check_flag_negative(self.a)
+        self.__check_flag_zero(self.a)
+        
+    def self._lsr_zeropage(self):
+        
+        
+        address = self.__read_word() 
+        aux = (self.memory[address] *2)%2 **8
+        self.memory[address]  =  aux
+        
+        #tem que adicionar o overflow
+        self.__check_flag_negative(self.a)
+        self.__check_flag_zero(self.a)
+        
+        return address
+    
+    def self._lsr_zeropage_x(self):
+        address = self.__read_word() 
+        aux = ((self.memory[address]+self.x) *2)%2 **8
+        self.memory[address]  =  aux
+        
+        #tem que adicionar o overflow
+        self.__check_flag_negative(self.a)
+        self.__check_flag_zero(self.a)
+        
+        return address
+
+
+
+    def self._lsr_absolute(self) :
+        self.a = (2*self.__read_word())% 2 ** 8
+        #tem que adicionar o overflow
+        self.__check_flag_negative(self.a)
+        self.__check_flag_zero(self.a)
+
+    def self._lsr_absolute_x(self):
+        self.a = (2*(self.__read_double+ self.x)) %2 **8    
+        #tem que adicionar o overflow
+        self.__check_flag_negative(self.a)
+        self.__check_flag_zero(self.a)
+
+        
