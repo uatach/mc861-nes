892cd93976e50419f96d621cb0f38c9dc0af0352
edson duarte <ra145892@students.ic.unicamp.br>
2019-09-28 01:17:44 -0300

    [tools] improved outputs

--- emulator/tools/test-all.sh
+++ emulator/tools/test-all.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
-for filepath in emulator/tst/*
+for filepath in emulator/bin/*
 do
   echo "-- $filepath"
-  filename=$(basename -- "$filepath")
-  name="${filename%.*}"
-
-  out=$(emulator/ext/asm6/asm6 emulator/tst/$filename emulator/bin/$name)
-
-  result=$(pynesemu emulator/bin/$name 2>/dev/null)
-  expected=$(cat emulator/res/$name.r)
-
-  diff -y --suppress-common-lines <(echo "$result") <(echo "$expected")
+  bash emulator/tools/test.sh $filepath 2>/dev/null
 done
