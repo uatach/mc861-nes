62f7e1ff62c54a9b03475e49df1a7a75d598aa6b
edson duarte <ra145892@students.ic.unicamp.br>
2019-10-11 11:26:34 -0300

    [ppu] attaching addresses on bus

--- emulator/src/ppu.py
+++ emulator/src/ppu.py
@@ -1,25 +1,28 @@
 import attr
 import pygame
 
+from .bus import Target
 from .register import Register
 
 
 @attr.s
-class PPU(object):
+class PPU(Target):
     bus = attr.ib()
     headless = attr.ib(False)
 
-    ppuctrl = Register()
-    ppumask = Register()
-    ppustatus = Register()
-    oamaddr = Register()
-    oamdata = Register()
-    ppuscroll = Register()
-    ppuaddr = Register()
-    ppudata = Register()
-    oamdma = Register()
-
     def setup(self):
+        self.registers = {}
+
+        self.bus.attach(0x2000, self)
+        self.bus.attach(0x2001, self)
+        self.bus.attach(0x2002, self)
+        self.bus.attach(0x2003, self)
+        self.bus.attach(0x2004, self)
+        self.bus.attach(0x2005, self)
+        self.bus.attach(0x2006, self)
+        self.bus.attach(0x2007, self)
+        self.bus.attach(0x4014, self)
+
         start = 0x2008
         mirrors = []
         for end in range(0x2010, 0x4001, 8):
@@ -33,7 +36,15 @@ class PPU(object):
             self.screen = pygame.display.set_mode([640, 480])
             pygame.display.set_caption("pynesemu")
 
+    def read(self, addr):
+        return self.registers.get(addr, 0)
+
+    def write(self, addr, value):
+        self.registers[addr] = value
+
     def step(self):
         if not self.headless:
             self.screen.fill((0, 0, 0))
             pygame.display.flip()
+
+        self.write(0x2002, 0b10000000)
